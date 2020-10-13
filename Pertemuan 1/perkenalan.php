faa2cd4948819b92fd18bfd9b920bb0ea9834bcc
defan-afk <56010825+defan-afk@users.noreply.github.com>
2020-10-13 10:52:17 +0700

    Add files via upload

---
+++ Pertemuan 1/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan</h1>";
+		echo "<h3>Nama Saya Getar jagat dewantara</h3>";
+		echo "<p>Saya tinggal di Slawi</p>";
+		echo "Hobi saya bermain musik";
+	}
+}
